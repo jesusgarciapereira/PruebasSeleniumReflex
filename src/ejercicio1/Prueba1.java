e1af3e804c1ea7e325a21f2b9504cb9bb0362047
Tragedia <Tragedia@LAPTOP-9QICE5TG>
2025-02-03 23:33:50 +0100

    Poco a poco

--- src/ejercicio1/Prueba1.java
+++ src/ejercicio1/Prueba1.java
@@ -1,6 +1,8 @@
 package ejercicio1;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -8,6 +10,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 class Prueba1 {
 
@@ -35,19 +39,38 @@ class Prueba1 {
 		
 		WebElement buscadores = driver1.findElement(By.id("buscadores"));
 		buscadores.click();
-		WebElement paginaBuscadores = driver1.findElement(By.id("page-buscadores"));
-		String buscadoresText = paginaBuscadores.getText();
+		String buscadoresText = buscadores.getText();
 		assertEquals("Buscadores", buscadoresText);
+		
+		 // Esperar a que el elemento con id "page-buscadores" esté presente en la página
+	    WebDriverWait wait = new WebDriverWait(driver1, Duration.ofSeconds(5));
+	    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("page-buscadores")));
+	    
+		WebElement paginaBuscadores = driver1.findElement(By.id("page-buscadores"));
+		String buscadoresPageText = paginaBuscadores.getText();
+		assertEquals("Buscadores", buscadoresPageText);
+		
+		
 	}
 	
 	@Test
 	void test3() {
 		driver1.get("http://localhost:3000/");
 		
-		
 		WebElement redes = driver1.findElement(By.id("redes"));
+		redes.click();
 		String redesText = redes.getText();
-		assertEquals("Redes sociales", redesText);
+		assertEquals("Redes Sociales", redesText);
+		
+		 // Esperar a que el elemento con id "page-redes" esté presente en la página
+	    WebDriverWait wait = new WebDriverWait(driver1, Duration.ofSeconds(5));
+	    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("page-redes")));
+	    
+		WebElement paginaRedes = driver1.findElement(By.id("page-redes"));
+		String buscadoresPageRedes = paginaRedes.getText();
+		assertEquals("Redes Sociales", buscadoresPageRedes);
+		
+
 	}
 
 }
