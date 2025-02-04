f4d1b93a73059784508cbdb8c3b795b26b988e1f
jgarcia <jgarcia@107-04>
2025-02-04 13:52:09 +0100

    Antes de probar el external

--- src/ejercicio1/Prueba1.java
+++ src/ejercicio1/Prueba1.java
@@ -23,154 +23,248 @@ class Prueba1 {
 	}
 
 	@Test
-	void test1() {
+	void testTituloEnlaces() {
 		driver1.get("http://localhost:3000/");
-		
-		WebElement indexCorrecto = driver1.findElement(By.id("enlaces"));
+
+		WebElement indexCorrecto = driver1.findElement(By.id("page-enlaces"));
 		String textoLogin = indexCorrecto.getText();
 		assertEquals("Enlaces favoritos", textoLogin);
 
 	}
 
 	@Test
-	void test2() {
+	void testTextoBuscadores() {
 		driver1.get("http://localhost:3000/");
-		
-		
-		WebElement buscadores = driver1.findElement(By.id("buscadores"));
-		buscadores.click();
-		String buscadoresText = buscadores.getText();
-		assertEquals("Buscadores", buscadoresText);
-		
-		 // Esperar a que el elemento con id "page-buscadores" esté presente en la página
-//	    WebDriverWait wait = new WebDriverWait(driver1, Duration.ofSeconds(5));
-//	    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("page-buscadores")));
-//	    
-//		WebElement paginaBuscadores = driver1.findElement(By.id("page-buscadores"));
-//		String buscadoresPageText = paginaBuscadores.getText();
-//		assertEquals("Buscadores", buscadoresPageText);
-		
-		
-	}
-	
-	@Test
-	void test3() {
-		driver1.get("http://localhost:3000/");
-		
-		WebElement redes = driver1.findElement(By.id("redes"));
-		redes.click();
-		String redesText = redes.getText();
-		assertEquals("Redes Sociales", redesText);
-		
-		 // Esperar a que el elemento con id "page-redes" esté presente en la página
-//	    WebDriverWait wait = new WebDriverWait(driver1, Duration.ofSeconds(5));
-//	    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("page-redes")));
-//	    
-//		WebElement paginaRedes = driver1.findElement(By.id("page-redes"));
-//		String buscadoresPageRedes = paginaRedes.getText();
-//		assertEquals("Redes Sociales", buscadoresPageRedes);
-		
 
-	}
-	
-	@Test
-	void test4() {
-		driver1.get("http://localhost:3000/buscadores");
-	    
-		WebElement paginaBuscadores = driver1.findElement(By.id("page-buscadores"));
+		WebElement paginaBuscadores = driver1.findElement(By.id("buscadores"));
 		String buscadoresPageText = paginaBuscadores.getText();
 		assertEquals("Buscadores", buscadoresPageText);
 	}
-	
+
 	@Test
-	void test5() {
-		driver1.get("http://localhost:3000/redes");
-			    
-		WebElement paginaRedes = driver1.findElement(By.id("page-redes"));
+	void testTextoRedes() {
+		driver1.get("http://localhost:3000/");
+
+		WebElement paginaRedes = driver1.findElement(By.id("redes"));
 		String buscadoresPageRedes = paginaRedes.getText();
 		assertEquals("Redes Sociales", buscadoresPageRedes);
-		
 
 	}
-	
+
 	@Test
-	void test6() {
-		driver1.get("http://localhost:3000/buscadores");
-		
+	void testNavegarBuscadoresTitulo() {
+		driver1.get("http://localhost:3000/");
+
+		WebElement buscadores = driver1.findElement(By.id("buscadores"));
+		buscadores.click();
+
+		// Esperar a que el elemento con id "page-buscadores" esté presente en la página
+		WebDriverWait wait = new WebDriverWait(driver1, Duration.ofSeconds(1));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("page-buscadores")));
+
+		WebElement paginaBuscadores = driver1.findElement(By.id("page-buscadores"));
+		String buscadoresPageText = paginaBuscadores.getText();
+		assertEquals("Buscadores", buscadoresPageText);
+
+	}
+
+	@Test
+	void testNavegarBuscadoresGoogle() {
+
+		driver1.get("http://localhost:3000/");
+
+		WebElement buscadores = driver1.findElement(By.id("buscadores"));
+		buscadores.click();
+
+		// Esperar a que el elemento con id "page-buscadores" esté presente en la página
+		WebDriverWait wait = new WebDriverWait(driver1, Duration.ofSeconds(1));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("page-buscadores")));
+
 		WebElement enlaceGoogle = driver1.findElement(By.id("google"));
-		enlaceGoogle.click();
+		// enlaceGoogle.click();
 		String enlaceGoogleText = enlaceGoogle.getText();
 		assertEquals("Google", enlaceGoogleText);
 	}
-	
+
 	@Test
-	void test7() {
-		driver1.get("http://localhost:3000/buscadores");
-		
+	void testNavegarBuscadoresBing() {
+		driver1.get("http://localhost:3000/");
+
+		WebElement buscadores = driver1.findElement(By.id("buscadores"));
+		buscadores.click();
+
+		// Esperar a que el elemento con id "page-buscadores" esté presente en la página
+		WebDriverWait wait = new WebDriverWait(driver1, Duration.ofSeconds(1));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("page-buscadores")));
+
 		WebElement enlaceBing = driver1.findElement(By.id("bing"));
-		enlaceBing.click();
+		// enlaceBing.click();
 		String enlaceBingText = enlaceBing.getText();
 		assertEquals("Bing", enlaceBingText);
 	}
-	
+
 	@Test
-	void test8() {
-		driver1.get("http://localhost:3000/buscadores");
-		
+	void testNavegarBuscadoresBaidu() {
+		driver1.get("http://localhost:3000/");
+
+		WebElement buscadores = driver1.findElement(By.id("buscadores"));
+		buscadores.click();
+
+		// Esperar a que el elemento con id "page-buscadores" esté presente en la página
+		WebDriverWait wait = new WebDriverWait(driver1, Duration.ofSeconds(1));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("page-buscadores")));
+
 		WebElement enlaceBaidu = driver1.findElement(By.id("baidu"));
-		enlaceBaidu.click();
+		// enlaceBaidu.click();
 		String enlaceBaiduText = enlaceBaidu.getText();
 		assertEquals("Baidu", enlaceBaiduText);
-	} 
-	
+	}
+
 	@Test
-	void test9() {
-		driver1.get("http://localhost:3000/buscadores");
-		
+	void testNavegarBuscadoresTextoVolver() {
+		driver1.get("http://localhost:3000/");
+
+		WebElement buscadores = driver1.findElement(By.id("buscadores"));
+		buscadores.click();
+
+		// Esperar a que el elemento con id "page-buscadores" esté presente en la página
+		WebDriverWait wait = new WebDriverWait(driver1, Duration.ofSeconds(1));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("page-buscadores")));
+
 		WebElement botonVolver = driver1.findElement(By.id("buscadores-volver"));
-		botonVolver.click();
+		// botonVolver.click();
 		String botonVolverText = botonVolver.getText();
 		assertEquals("Volver", botonVolverText);
 	}
-	
+
 	@Test
-	void test10() {
-		driver1.get("http://localhost:3000/redes");
-		
+	void testNavegarBuscadoresNavegarVolver() {
+		driver1.get("http://localhost:3000/");
+
+		WebElement buscadores = driver1.findElement(By.id("buscadores"));
+		buscadores.click();
+
+		// Esperar a que el elemento con id "page-buscadores" esté presente en la página
+		WebDriverWait wait = new WebDriverWait(driver1, Duration.ofSeconds(1));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("page-buscadores")));
+
+		WebElement botonVolver = driver1.findElement(By.id("buscadores-volver"));
+		botonVolver.click();
+
+		// Esperar a que el elemento con id "page-enlaces" esté presente en la página
+		wait = new WebDriverWait(driver1, Duration.ofSeconds(1));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("page-enlaces")));
+
+		WebElement indexCorrecto = driver1.findElement(By.id("page-enlaces"));
+		String textoLogin = indexCorrecto.getText();
+		assertEquals("Enlaces favoritos", textoLogin);
+	}
+
+	@Test
+	void testNavegarRedesTitulo() {
+		driver1.get("http://localhost:3000/");
+
+		WebElement redes = driver1.findElement(By.id("redes"));
+		redes.click();
+
+		// Esperar a que el elemento con id "page-redes" esté presente en la página
+		WebDriverWait wait = new WebDriverWait(driver1, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("page-redes")));
+
+		WebElement paginaRedes = driver1.findElement(By.id("page-redes"));
+		String buscadoresPageRedes = paginaRedes.getText();
+		assertEquals("Redes Sociales", buscadoresPageRedes);
+
+	}
+
+	@Test
+	void testNavegarRedesInstagram() {
+		driver1.get("http://localhost:3000/");
+
+		WebElement redes = driver1.findElement(By.id("redes"));
+		redes.click();
+
+		// Esperar a que el elemento con id "page-redes" esté presente en la página
+		WebDriverWait wait = new WebDriverWait(driver1, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("page-redes")));
+
 		WebElement enlaceInstagram = driver1.findElement(By.id("instagram"));
-		enlaceInstagram.click();
+		// enlaceInstagram.click();
 		String enlaceInstagramText = enlaceInstagram.getText();
 		assertEquals("Instagram", enlaceInstagramText);
 	}
-	
+
 	@Test
-	void test11() {
-		driver1.get("http://localhost:3000/redes");
-		
+	void testNavegarRedesTiktok() {
+		driver1.get("http://localhost:3000/");
+
+		WebElement redes = driver1.findElement(By.id("redes"));
+		redes.click();
+
+		// Esperar a que el elemento con id "page-redes" esté presente en la página
+		WebDriverWait wait = new WebDriverWait(driver1, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("page-redes")));
+
 		WebElement enlaceTikTok = driver1.findElement(By.id("tiktok"));
-		enlaceTikTok.click();
+		// enlaceTikTok.click();
 		String enlaceTikTokText = enlaceTikTok.getText();
 		assertEquals("TikTok", enlaceTikTokText);
 	}
-	
+
 	@Test
-	void test12() {
-		driver1.get("http://localhost:3000/redes");
-		
+	void testNavegarRedesFacebook() {
+		driver1.get("http://localhost:3000/");
+
+		WebElement redes = driver1.findElement(By.id("redes"));
+		redes.click();
+
+		// Esperar a que el elemento con id "page-redes" esté presente en la página
+		WebDriverWait wait = new WebDriverWait(driver1, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("page-redes")));
+
 		WebElement enlaceFacebook = driver1.findElement(By.id("facebook"));
-		enlaceFacebook.click();
+		// enlaceFacebook.click();
 		String enlaceFacebookText = enlaceFacebook.getText();
 		assertEquals("Facebook", enlaceFacebookText);
-	} 
-	
+	}
+
 	@Test
-	void test13() {
-		driver1.get("http://localhost:3000/redes");
-		
+	void testNavegarRedesTextoVolver() {
+		driver1.get("http://localhost:3000/");
+
+		WebElement redes = driver1.findElement(By.id("redes"));
+		redes.click();
+
+		// Esperar a que el elemento con id "page-redes" esté presente en la página
+		WebDriverWait wait = new WebDriverWait(driver1, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("page-redes")));
+
 		WebElement botonVolver = driver1.findElement(By.id("redes-volver"));
-		botonVolver.click();
 		String botonVolverText = botonVolver.getText();
 		assertEquals("Volver", botonVolverText);
+	}
+
+	@Test
+	void testNavegarRedesNavegarVolver() {
+		driver1.get("http://localhost:3000/");
+
+		WebElement redes = driver1.findElement(By.id("redes"));
+		redes.click();
+
+		// Esperar a que el elemento con id "page-redes" esté presente en la página
+		WebDriverWait wait = new WebDriverWait(driver1, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("page-redes")));
+
+		WebElement botonVolver = driver1.findElement(By.id("redes-volver"));
+		botonVolver.click();
+
+		// Esperar a que el elemento con id "page-enlaces" esté presente en la página
+		wait = new WebDriverWait(driver1, Duration.ofSeconds(1));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("page-enlaces")));
+
+		WebElement indexCorrecto = driver1.findElement(By.id("page-enlaces"));
+		String textoLogin = indexCorrecto.getText();
+		assertEquals("Enlaces favoritos", textoLogin);
 	}
 
 }
